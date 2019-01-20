720508e45028ee09b06a0cc98e25263d2d119614
ramkrishnan <iramkrish95@gmail.com>
2019-01-20 18:20:50 +0530

    server file

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var socket = require("socket.io");
+
+var app = express();
+
+const server = app.listen("4000", () => {
+  console.log("server started and listening on port 4000");
+});
+
+// static file from public directory
+app.use(express.static("public"));
+
+// socket setuo
+const io = new socket(server);
+
+io.on("connection", socket => {
+  console.log("Made connection , id : ", socket.id);
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data);
+  });
+});
